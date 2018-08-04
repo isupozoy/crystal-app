e6d0483d822f040de0aabcb7195edfa2d9729121
Nicholas E <isupozoy@gmail.com>
2018-08-04 04:27:02 -0400

    This is where the user visits.

---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Crystal App - Create crystal clear web applications with minimal configuration and easy to use tools.
+ *
+ * @author Nicholas E. <https://github.com/isupozoy> Maintainer.
+ * @link   <https://github.com/isupozoy/crystal-app> Github Repository.
+ */
+
+require __DIR__ . '/../bootstrap.php';
